64b8bd51ea3a75fb6573016f05f1821e2eda8d5a
Alex Whirley <alexwhirley@gmail.com>
2020-05-11 16:59:57 -0500

    adding typography plugin

--- basic-site/src/pages/index.js
+++ basic-site/src/pages/index.js
@@ -9,11 +9,14 @@ const IndexPage = () => (
   <Layout>
     <SEO title="home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+    
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
