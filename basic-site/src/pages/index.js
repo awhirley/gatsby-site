2ade7eb7c4b804db1b559206dc5068a47d0c11d2
Alex Whirley <alexwhirley@gmail.com>
2020-05-12 20:01:01 -0500

    css fixed!

--- basic-site/src/pages/index.js
+++ basic-site/src/pages/index.js
@@ -5,24 +5,28 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import "../components/homepage.css"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="home" />
-    <div class='container-fluid row justify-content-center'>
-      <div class='col-sm-4'>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <div class='grid-container'>
+
+      <div class='image_item'>
+        <div class='image_position'>
           <Image />
         </div>
       </div>
-      <div class='col-sm-4'>
+
+      <div class='intro_item'>
         <h1>Hi people</h1>
 
         <p>Welcome to your new Gatsby site.</p>
         <p>Now go build something great.</p>
       
         <Link to="/page-2/">Go to page 2</Link>
-        
       </div>
+
     </div>
   </Layout>
 )
